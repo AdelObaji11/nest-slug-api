adedc75f2b969cfe1f22f3d7db63a56f17dec49d
Adel Obaji <adelobaji23@gmail.com>
2026-04-13 10:11:28 -0900

    define slug library into NestJS app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { slug } from 'slug-generator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const text = 'Hello Nestjs';
+    const slugify = slug(text);
+    return slugify;
   }
 }
